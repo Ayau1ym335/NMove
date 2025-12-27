674f8792e6568c53959721f42a139f6a396d0e4a
Ayau1ym335 <aaulymbalginbaeva@gmail.com>
2025-12-27 17:38:12 +0500

    gitignore added

---
+++ backend/app/module.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from typing import List, Literal
+import uvicorn
+app = FastAPI()
+
+class IMUSample(BaseModel):
+    device_mac: str
+    session_id: str
+    device_pos: Literal['thigh', 'shin']
+    timestamp: int
+    accel_x: float
+    accel_y: float
+    accel_z: float
+    gyro_x: float
+    gyro_y: float
+    gyro_z: float
+@app.post('/ingest', status_code=200)
+async def ingest(sample: List[IMUSample]):
+    if not sample:
+        raise HTTPException(status_code=400, detail="Sample is empty")
+
+    thigh_count = sum(1 for s in sample if s.device_pos == 'thigh')
+    shin_count = sum(1 for s in sample if s.device_pos == 'shin')
+    print(f' shin_count: {shin_count}, thigh_count: {thigh_count}')
+    return {'received': len(sample), 'status' : 'success'}
+
+
+if __name__ == "__main__":
+    uvicorn.run("module:app", port=8000, reload=True)
--- data/connect.py
+++ data/connect.py
@@ -1,20 +1,113 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import (Column, Integer,String,CheckConstraint,
+                        DateTime, Float, Boolean,  Enum as SQLEnum, text)
+from datetime import datetime, timedelta
+from sqlalchemy.orm import declarative_base
+import enum
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-# 1. Define your connection string
-# Format: postgresql://username:password@host:port/database
-DATABASE_URL = "postgresql://postgres:Ayaulym^2011@localhost:5432/stridex"
+DATABASE_URL = "postgresql+asyncpg://postgres:Ayaulym^2011@localhost:5433/stridex"
+engine = create_async_engine(DATABASE_URL, echo=True)
 
-# 2. Create the Engine (The central source of the connection)
-engine = create_engine(DATABASE_URL)
+session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
 
-def check_connection():
-    try:
-        # 3. Connect and execute
-        with engine.connect() as connection:
-            result = connection.execute(text("SELECT version();"))
-            print(f"Success! Database version: {result.fetchone()[0]}")
-    except Exception as e:
-        print(f"Connection failed: {e}")
+class GenderEnum(enum.Enum):
+    MALE = 'male'
+    FEMALE = 'female'
 
-if __name__ == "__main__":
-    check_connection()
+class StatusEnum(enum.Enum):
+    THIGH = 'thigh'
+    SHIN = 'shin'
+
+class SideEnum(enum.Enum):
+    LEFT = 'left'
+    RIGHT = 'right'
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(String(8), CheckConstraint('length(code) = 8'), index=True)
+    name = Column(String, nullable=False, comment='Введите реальное ФИО')
+    age = Column(Integer, CheckConstraint('age > 0 AND age < 120'))
+    gender = Column(SQLEnum(GenderEnum))
+    weight = Column(Float)
+    height = Column(Float)
+    shoe_size = Column(Float, comment="Размер обуви (RU)")
+    has_flat_feet = Column(Boolean, default=False, comment="Наличие плоскостопия")
+    injury_history = Column(String(500), comment="Описание травм и стороны поражения")
+    dominant_leg = Column(SQLEnum(SideEnum), comment="Ведущая нога")
+    placed_leg = Column(SQLEnum(SideEnum), comment="Нога на которой расположен модуль")
+    created_at = Column(DateTime, default=lambda: datetime.now())
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+    password = Column(
+        String(12),
+        nullable=False,
+    )
+
+
+    __table_args__ = (
+        CheckConstraint('length(password) <= 12', name='password_max_length'),
+        CheckConstraint("email LIKE '%@%'", name="check_email_format"),
+        CheckConstraint('height > 80 AND height < 210', name='check_height_range'),
+        CheckConstraint('weight > 20 AND weight < 200', name='check_weight_range')
+    )
+
+class RawData(Base):
+    __tablename__ = "raw_data"
+    id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, index=True, nullable=False)
+    timestamp = Column(DateTime, default=lambda: datetime.now())
+    #thigh
+    ta_x = Column(Float)
+    ta_y = Column(Float)
+    ta_z = Column(Float)
+    tg_x = Column(Float)
+    tg_y = Column(Float)
+    tg_z = Column(Float)
+    #shin
+    sa_x = Column(Float)
+    sa_y = Column(Float)
+    sa_z = Column(Float)
+    sg_x = Column(Float)
+    sg_y = Column(Float)
+    sg_z = Column(Float)
+    expired_at = Column(DateTime, default=lambda: datetime.now() + timedelta(days=4))
+
+class StepMetrics(Base):
+    __tablename__ = "step_metrics"
+    id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, index=True, nullable=False)
+    timestamp = Column(DateTime, default=lambda: datetime.now())
+
+class Session(Base):
+    __tablename__ = "session"
+
+class DailyData(Base):
+    __tablename__ = "daily_data"
+
+class MonthData(Base):
+    __tablename__ = "month_data"
+
+async def hypertable(engine, base):
+    async with engine.begin() as conn:
+        await conn.run_sync(base.metadata.create_all)
+
+        # 2. Iterate through all tables defined in your code
+        for table_name, table_object in base.metadata.tables.items():
+            # Check if we tagged this table in the model definition
+            hyper_info = table_object.info.get("is_hypertable")
+            time_col = table_object.info.get("time_column")
+
+            if hyper_info and time_col:
+                print(f"Converting {table_name} to hypertable via column {time_col}...")
+
+                # Execute the conversion
+                await conn.execute(text(
+                    f"SELECT create_hypertable('{table_name}', '{time_col}', if_not_exists => TRUE);"
+                ))
+
+    print("All hypertables processed.")
